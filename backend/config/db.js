33088d0e7c3368d77346a4feb34131b7c14277d6
hieutrung10042 <txxh1004@gmail.com>
2026-05-20 19:36:07 +0700

    fix: add ssl configuration for aiven database

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,13 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 27695, 
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false 
+    }
 });
 
 // Chuyển sang dùng Promise để code gọn hơn với async/await
